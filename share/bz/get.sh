f2ed8addc603578c4ab7682129085f2078ba2bc0
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-07-31 18:02:29 +0000

    cleanup get

--- share/bz/get.sh
+++ share/bz/get.sh
@@ -1,4 +1,49 @@
+usage () {
+  cat <<EOF
+Usage: bz get pr
+       bz get -h
+
+Options:
+    -h    -- this help message
+
+Args:
+    pr    -- pr number
+EOF
+
+  exit 1
+}
+
+get () {
+  local pr=$1
+
+  local d=$(_pr_dir $pr)
+
+  get_pr $d $pr
+  get_attachment $d $pr
+}
+
+get_pr () {
+  local d=$1
+  local pr=$2
+
+  backend_get_pr $pr > $d/pr
+}
+
+get_attachment () {
+  local d=$1
+  local pr=$2
+
+  backend_get_attachment $pr > $d/patch
+}
+
 . ${BZ_BACKENDDIR}/get.sh
+
+while getopts h FLAG; do
+  case ${FLAG} in
+    h) usage ;;
+  esac
+done
+shift $(($OPTIND-1))
 
 pr=$1
 
--- share/bz/pybugz/get.sh
+++ share/bz/pybugz/get.sh
@@ -1,31 +1,27 @@
 . ${BZ_SCRIPTDIR}/_util.sh
 
-get () {
+backend_get_pr () {
   local pr=$1
 
-  local d=$(_pr_dir $pr)
-
-  _get_pr $d $pr
-  _get_attachment $d $pr
+  $bugz get $pr
 }
 
-_get_pr () {
-  local d=$1
-  local pr=$2
-
-  bugz get $pr > $d/pr
-}
-
-_get_attachment () {
-  local d=$1
-  local pr=$2
+backend_get_attachment () {
+  local pr=$1
 
   local a_cnt=$(grep ^Attachments $d/pr | cut -d: -f2 | sed -e 's, ,,g')
 
-  if [ $a_cnt -eq 0 ]; then
-    echo "No Attachments Found for $pr"
-  else
-    local id=$(grep "\[Attachment\]" $d/pr | egrep -i 'shar|diff|patch|shell|update' | awk '{ print $2 }' | sed -e 's,\[,,' -e 's,\],,' | sort -n | tail -1 )
-    fetch -q -o $d/patch "https://bz-attachments.freebsd.org/attachment.cgi?id=$id"
+  if [ $a_cnt -gt 0 ]; then
+      local id=$(
+        grep "\[Attachment\]" $d/pr | \
+            egrep -i 'shar|diff|patch|shell|update' | \
+            awk '{ print $2 }' | \
+            sed -e 's,\[,,' -e 's,\],,' | \
+            sort -n | \
+            tail -1
+            )
+      ## XXX: output is easier to parse then bugz attachment -v
+      ## XXX: otherbackends shouldn't need this url
+    fetch -q -o - "https://bz-attachments.freebsd.org/attachment.cgi?id=$id"
   fi
 }
