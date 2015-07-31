c41f429b7de6d88025decdb7fb8927e143cdf1f9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-07-31 16:30:48 +0000

    cleanup init

--- share/bz/init.sh
+++ share/bz/init.sh
@@ -1,3 +1,52 @@
+bzinit () {
+
+  local user
+  local email
+  local password
+
+  echo -n "FreeBSD Bugzilla User [$USER]: "
+  read user
+  [ -z "$user" ] && user=$USER
+
+  echo -n "E-Mail to use for submissions [$USER@FreeBSD.org]: "
+  read email
+  [ -z "$email" ] && email="$USER@FreeBSD.org"
+
+  echo -n "FreeBSD Bugzilla Password: "
+  stty -echo
+  trap 'stty echo' EXIT
+  read password
+  stty echo
+  trap - EXIT
+  echo
+  [ -z "$password" ] && echo "Blank Password." && exit 1
+
+  cat <<EOF > $HOME/.fbcrc
+REPORTER=$email
+EOF
+
+  local url="https://bugs.freebsd.org/bugzilla/xmlrpc.cgi"
+  backend_init $url $user $password
+}
+
+usage () {
+  cat <<EOF
+Usage bz init [-h]
+
+Options:
+    -h    -- this help message
+
+Will initilaize config files in ~ for itself and ${BZ_BACKEND}.
+EOF
+}
+
 . ${BZ_BACKENDDIR}/init.sh
+
+while getopts h FLAG; do
+  case ${FLAG} in
+    h) usage ;;
+  esac
+done
+shift $(($OPTIND-1))
 
 bzinit
--- share/bz/pybugz/init.sh
+++ share/bz/pybugz/init.sh
@@ -1,31 +1,9 @@
-. ${BZ_SCRIPTDIR}/_util.sh
+backend_init () {
+  local url=$1
+  local user=$2
+  local pass=$3
 
-bzinit () {
-
-  local user
-  local email
-  local password
-
-  echo -n "FreeBSD Bugzilla User [$USER]: "
-  read user
-  [ -z "$user" ] && user=$USER
-
-  echo -n "E-Mail to use for submissions [$USER@FreeBSD.org]: "
-  read email
-  [ -z "$email" ] && email="$USER@FreeBSD.org"
-
-  echo -n "FreeBSD Bugzilla Password: "
-  stty -echo
-  trap 'stty echo' EXIT
-  read password
-  stty echo
-  trap - EXIT
-  echo
-  [ -z "$password" ] && echo "Blank Password." && exit 1
-
-  local url="https://bugs.freebsd.org/bugzilla/xmlrpc.cgi"
-
-  cat <<EOF > $HOME/.bugzrc
+    cat <<EOF > $HOME/.bugzrc
 [default]
 connection=FBSD
 
@@ -33,9 +11,5 @@ connection=FBSD
 base=$url
 user=$user
 password=$password
-EOF
-
-  cat <<EOF > $HOME/.fbcrc
-REPORTER=$email
 EOF
 }
