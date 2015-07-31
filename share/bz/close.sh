358c5a88bfc8d19fb53026b106223f248b23f1d6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-07-31 19:34:25 +0000

    cleanuup close

--- share/bz/close.sh
+++ share/bz/close.sh
@@ -1,5 +1,50 @@
+usage () {
+  cat <<EOF
+Usage: bz close [-c comment] [-e] pr
+       bz take -h
+
+Options:
+    -c    -- optional comment (default none)
+    -e    -- optional spawn $EDITOR
+    -h    -- this help message
+
+Args:
+    pr    -- pr number
+
+Will mark pr as Closed, FIXED and by default no comment.
+EOF
+
+  exit 1
+}
+
+close () {
+  local pr=$1
+  local f_e=$2
+  local comment="$3"
+
+  local comment_file=$(mktemp -q /tmp/_bz-comment.txt.XXXXXX)
+  if [ $f_e -eq 1 ]; then
+    echo "$comment" > $comment_file
+  else
+    $EDITOR $comment_file >/dev/tty
+  fi
+
+  backend_close $pr
+}
+
 . ${BZ_BACKENDDIR}/close.sh
+
+comment=
+f_e=0
+while getopts c:eh FLAG; do
+  case ${FLAG} in
+    c) comment="$OPTARG" ;;
+    e) f_e=1 ;;
+    h) usage ;;
+  esac
+done
+shift $(($OPTIND-1))
 
 pr=$1
 
-close $pr
+close $pr $f_e "$comment"
--- share/bz/pybugz/close.sh
+++ share/bz/pybugz/close.sh
@@ -1,5 +1,10 @@
-close () {
-  local pr=$1
+. ${BZ_SCRIPTDIR}/_util.sh
 
-  bugz modify -s "Closed" -r "FIXED" $pr
+backend_close () {
+  local pr=$1
+  local comment="$2"
+
+  [ -n "$comment" ] && comment="-c \"$comment\""
+
+  $bugz modify -s "Closed" -r "FIXED" $comment $pr
 }
