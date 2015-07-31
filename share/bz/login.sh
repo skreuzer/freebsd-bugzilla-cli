6b9190faffb771245abc865e16c813281a549182
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-07-31 16:43:42 +0000

    cleanup login

--- share/bz/login.sh
+++ share/bz/login.sh
@@ -1,3 +1,30 @@
+usage () {
+    cat <<EOF
+Usage bz login [-h]
+
+Options:
+    -h    -- this help message
+
+Will login to bugzilla instance (which is required for write actions).
+All subsequent command args will be sent to the $BZ_BACKEND; however,
+none should be neede by default.
+EOF
+
+    exit 1
+}
+
+login () {
+  local arg_str="$1"
+  backend_login "$arg_str"
+}
+
 . ${BZ_BACKENDDIR}/login.sh
 
-login "$@"
+while getopts h FLAG; do
+  case ${FLAG} in
+    h) usage ;;
+  esac
+done
+shift $(($OPTIND-1))
+
+login "$*"
--- share/bz/pybugz/login.sh
+++ share/bz/pybugz/login.sh
@@ -1,3 +1,7 @@
-login () {
-  bugz login "$@"
+. ${BZ_SCRIPTDIR}/_util.sh
+
+backend_login () {
+  local arg_str="$1"
+
+  $bugz login $@
 }
