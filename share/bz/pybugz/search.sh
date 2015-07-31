55337fe9e7fec7c40a0e7ec986125557a3e311e4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-07-31 16:58:00 +0000

    cleanup search

--- share/bz/pybugz/search.sh
+++ share/bz/pybugz/search.sh
@@ -1,12 +1,14 @@
 . ${BZ_SCRIPTDIR}/_util.sh
 
-search () {
-  $bugz \
+backend_search () {
+  local arg_str="$1"
+
+  $bugz                            \
       --encoding=utf8              \
       search                       \
       --product "Ports & Packages" \
       -s New                       \
       -s Open                      \
       -s "In Progress"             \
-      "$*"
+      $arg_str
 }
--- share/bz/search.sh
+++ share/bz/search.sh
@@ -1,3 +1,34 @@
+usage () {
+  cat <<EOF
+Usage: bz search [-h] [-- backend search args]
+
+Optional:
+    -h    -- this help message
+
+Will search by default for all new,open,in progress bugs in ports.
+All subsequent command args will be sent to the $BZ_BACKEND.
+
+i.e.
+  # pybugz backend
+  bz search -- -apgollucci@FreeBSD.org
+EOF
+
+  exit 1
+}
+
+search () {
+  local arg_str="$1"
+
+  backend_search "$arg_str"
+}
+
 . ${BZ_BACKENDDIR}/search.sh
+
+while getopts h FLAG; do
+  case ${FLAG} in
+    h) usage ;;
+  esac
+done
+shift $(($OPTIND-1))
 
 search "$@"
