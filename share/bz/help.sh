f31a65c544f4e08a9850807b38c32d8bad464356
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-08-01 02:05:39 +0000

    new help menu

--- share/bz/help.sh
+++ share/bz/help.sh
@@ -1,3 +1,17 @@
-cmds=$(ls -1 ${BZ_SCRIPTDIR} | grep .sh$ | grep -v ^_ | sed -e 's,.sh, ,g' -e 's,|$,,')
-
-echo $cmds
+cat <<EOF
+close        - close a pr
+comment      - comment on a pr
+get          - get a pr locally, needed for patch
+help         - display this message
+init         - collect info and initialize config files
+inprog       - mark a pr in progress
+login        - login to bugzilla
+overto       - reassign a pr to someone
+patch        - patch a port
+search       - search prs
+submit       - submit a pr with shar/patch for a port
+take         - assign a pr to yourself
+timeout      - comment on a pr stating maintainer and timeout duration
+top          - show top assignee / reporters
+version      - display software version
+EOF
