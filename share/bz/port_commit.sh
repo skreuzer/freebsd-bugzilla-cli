8c5bac6f55ae5e80736ccc84ac3bbf354fc7687b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-08-19 19:08:10 +0000

    add -m, multiport support

--- share/bz/port_commit.sh
+++ share/bz/port_commit.sh
@@ -1,11 +1,12 @@
 usage() {
   cat <<EOF
-Usage: bz commit [-c] [-n] pr
+Usage: bz commit [-c] [-m] [-n] pr
        bz commit -h
 
 Options:
     -c     -- also close the pr
     -h     -- this help message
+    -m     -- assume multi port, include any changed in PORTSDIR
     -n     -- dry run, do not actually commit
 
 Args:
@@ -18,6 +19,7 @@ commit () {
   local pr=$1
   local f_n=$2
   local f_c=$3
+  local f_m=$4
 
   ## sync copy
   ${ME} get -n $pr
@@ -27,28 +29,30 @@ commit () {
 
   ## which port
   local d=$(_pr_dir $pr)
-  local port_dir=$(_port_from_pr $d)
+  local port_dir
+  [ $f_m -eq 0 ] && port_dir=$(_port_from_pr $d)
+
   local commit_file=$(mktemp -q /tmp/_bzcommit-comment.txt.XXXXXX)
 
   # yep, make it easy
-  _make_commit_msg $d $port_dir $commit_file
+  _make_commit_msg $d "$port_dir" $commit_file
 
   ## cleanup port_dir
   _clean_port_dir $port_dir
-  _remove_empty_files $port_dir $vc
-  local newfiles=$(_add_new_files $port_dir $vc)
+  _remove_empty_files "$port_dir" $vc
+  local newfiles=$(_add_new_files "$port_dir" $vc)
 
   ## Are you sure?
-  _preview_commit $vc $port_dir $commit_file
+  _preview_commit $vc "$port_dir" $commit_file
 
   ## Proceed?
   _confirm $commit_file
 
   ## Do it!
-  _doit $f_n $vc $port_dir $commit_file
+  _doit $f_n $vc "$port_dir" $commit_file
 
   ## Post Do it
-  _post_doit $vc $port_dir $newfiles
+  _post_doit $vc "$port_dir" $newfiles
 
   ## cleanup
   [ $f_n -eq 0 -a $f_c -eq 1 ] && ${ME} close $pr
@@ -61,9 +65,13 @@ _make_commit_msg () {
   local port_dir=$2
   local commit_file=$3
 
-  local submitter=$(_submitter_from_pr $d)
-  local submitter_short=$(echo $submitter | sed -e 's,FreeBSD.org,,i')
-  local maintainer=$(_maintainer_from_port $port_dir)
+  local submitter
+  local submitter_short
+  local maintainer
+
+  [ -n "$port_dir" ] && submitter=$(_submitter_from_pr $d)
+  [ -n "$port_dir" ] && submitter_short=$(echo $submitter | sed -e 's,FreeBSD.org,,i')
+  [ -n "$port_dir" ] && maintainer=$(_maintainer_from_port $port_dir)
 
   . $HOME/.fbcrc
 
@@ -77,24 +85,27 @@ _make_commit_msg () {
   echo >>                            $commit_file
   echo "PR:                  $pr" >> $commit_file
 
-  if [ "$REPORTER" != "$submitter" ]; then
-    if [ "$submitter" = "$maintainer" ]; then
-      echo "Submitted by:        $submitter_short (maintainer)" >> $commit_file
-    else
-      echo "Submitted by:        $submitter_short" >> $commit_file
+  if [ -n "$port_dir" ]; then
+    if [ "$REPORTER" != "$submitter" ]; then
+      if [ "$submitter" = "$maintainer" ]; then
+        echo "Submitted by:        $submitter_short (maintainer)" >> $commit_file
+      else
+        echo "Submitted by:        $submitter_short" >> $commit_file
+      fi
     fi
-  fi
 
-  if [ "$submitter" != "$maintainer" ]; then
-    local timeout_str=$(_timeout_from_pr $d)
-    if [ -n "$timeout_str" ]; then
-      echo "Approved by:         $timeout_str" >> $commit_file
-    else
-      if [ "$maintainer" != "ports@FreeBSD.org" ]; then
-        echo "Approved by:         $maintainer (maintainer)" >> $commit_file
+    if [ "$submitter" != "$maintainer" ]; then
+      local timeout_str=$(_timeout_from_pr $d)
+      if [ -n "$timeout_str" ]; then
+        echo "Approved by:         $timeout_str" >> $commit_file
+      else
+        if [ "$maintainer" != "ports@FreeBSD.org" ]; then
+          echo "Approved by:         $maintainer (maintainer)" >> $commit_file
+        fi
       fi
     fi
   fi
+
   if [ -n "$SPONSORED_BY" ]; then
     echo "Sponsored by:        $SPONSORED_BY" >> $commit_file
   fi
@@ -112,8 +123,7 @@ _clean_port_dir () {
            \( -type f -o -type l \) -a \
            \( -name "*.bak" -o -name "*~" -o -name ".\#*" -o -name "\#*" \
                  -o -name "*.rej" -o -name "svn-commit.*" -o -name "*.orig" \
-                 -o -name "*-i" -o -name "*-E" -o -name "*-e" -o -name "*.tmp" \
-                 -o -name "=~+*" \
+                 -o -name "*.tmp" -o -name "=~+*" \
            \) \
            -print -exec rm -f "{}" \;
   )
@@ -124,7 +134,7 @@ _remove_empty_files () {
   local vc=$2
 
   if [ "$vc" = "git" ]; then
-    ( cd $PORTSDIR/$port_dir ; find . -type f -empty | xargs git rm -f )
+    ( cd $PORTSDIR/$port_dir ; find . -type f -empty | egrep -v 'misc/freebsd-doc-|devel/linux-c6-qt47' | xargs git rm -f )
   else
     ( cd $PORTSDIR/$port_dir ; find . -type f -empty | xargs svn rm -f  )
   fi
@@ -164,6 +174,7 @@ _preview_commit () {
   fi
   echo "------------------------------COMMIT MESSAGE----------------------------------"
   cat $commit_file
+  echo
   echo "=============================================================================="
   echo
 }
@@ -221,10 +232,12 @@ _post_doit () {
 . ${BZ_SCRIPTDIR}/_util.sh
 
 f_c=0
+f_m=0
 f_n=0
 while getopts chn FLAG; do
   case ${FLAG} in
     c) f_c=1 ;;
+    m) f_m=1 ;;
     n) f_n=1 ;;
     *|h) usage ;;
   esac
@@ -233,4 +246,4 @@ shift $(($OPTIND-1))
 
 pr=$1
 
-commit $pr $f_n $f_c
+commit $pr $f_n $f_c $f_m
