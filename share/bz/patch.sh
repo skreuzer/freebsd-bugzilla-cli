6e264acfbddbf331ecc2619055fd6bb69540b841
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-08-31 20:28:35 +0000

    handle new get format, figure out how to extract shar better

--- share/bz/patch.sh
+++ share/bz/patch.sh
@@ -26,23 +26,30 @@ bzpatch () {
 
   local d=$(_pr_dir $pr)
 
+  if [ -f $d/shar ]; then
+    _bzpatch_shar $d $d/shar
+    f_m=1
+  fi
+
   local is_shar=$(head -1 $d/patch | grep -c "# This is a shell archive.")
 
   if [ $is_shar -eq 1 ]; then
-    _bzpatch_shar $d
+    _bzpatch_shar $d $d/patch
   else
-    _bzpatch_patch $d $f_m
+    _bzpatch_patch $d/patch $f_m
   fi
 }
 
 _bzpatch_patch () {
-  local d=$1
+  local file=$1
   local f_m=$2
 
-  local l=$(egrep "^Index:|^diff |^--- " $d/patch | head -1 | awk '{ print gsub(/\//,"") }')
+  local port=$(_port_from_pr $d)
+
+  local l=$(egrep "^Index:|^diff |^--- " $file | head -1 | awk '{ print gsub(/\//,"") }')
   local p
 
-  if grep -q ^diff $d/patch; then
+  if grep -q ^diff $file; then
     p="-p$(($l/2))"
   else
     if [ -n "$l" -a -n "$port" ]; then
@@ -51,34 +58,36 @@ _bzpatch_patch () {
   fi
 
   if [ $f_m -eq 1 ]; then
-    (cd $PORTSDIR ; patch $p < $d/patch)
+    (cd $PORTSDIR ; patch < $file)
   else
-    local port=$(_port_from_pr $d)
-    (cd $PORTSDIR/$port ; patch $p < $d/patch)
+    (cd $PORTSDIR/$port ; patch $p < $file)
   fi
 }
 
 _bzpatch_shar () {
   local d=$1
+  local file=$2
 
-  local port=$(_port_from_pr $d)
+  local l=$(grep "/Makefile$" $file | head -1 | awk '{ print gsub(/\//,"") }')
 
-  local l=$(grep /Makefile $d/patch | head -1 | awk '{ print gsub(/\//,"") }')
   if [ $l -eq 1 ]; then
-    local category=$(awk '/^XCATEGORIES=/ { print $2 }' $d/patch)
+    local category=$(awk '/^XCATEGORIES=/ { print $2 }' $file | head -1)
     mkdir -p $d/extract
-    (cd $d/extract ; sh $d/patch)
-    mkdir -p $PORTSDIR/$category/$port
-    cp -R $d/extract/* $PORTSDIR/$category/$port
+    (cd $d/extract ; sh $file)
+    if echo $port | grep -q /; then
+      mkdir -p $PORTSDIR/$port
+      cp -R $d/extract/* $PORTSDIR/
+    else
+      mkdir -p $PORTSDIR/$category/$port
+      cp -R $d/extract/* $PORTSDIR/$category/$port
+    fi
   else
-    sed -i '' -e 's,/usr/ports/,,' $d/patch
-    (cd $PORTSDIR ; sh $d/patch)
+    sed -i '' -e 's,/usr/ports/,,' $file
+    (cd $PORTSDIR ; sh $file)
   fi
 }
 
 . ${BZ_SCRIPTDIR}/_util.sh
-
-# XXX: no backend needed
 
 f_m=0
 while getopts hm FLAG; do
